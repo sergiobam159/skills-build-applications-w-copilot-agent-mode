64b561a07441bc62234d8652ebcf9447959a84ec
Sergio Henry Silva Navarro <43280698+sergiobam159@users.noreply.github.com>
2025-09-10 20:27:15 +0000

    Actualizar configuración de ALLOWED_HOSTS y ajustar la URL base en la vista api_root para soportar entornos de desarrollo en Codespaces.

--- octofit-tracker/backend/octofit_tracker/settings.py
+++ octofit-tracker/backend/octofit_tracker/settings.py
@@ -25,7 +25,11 @@ SECRET_KEY = 'django-insecure-7&9y5vxz18$l=b@3)r8(tx3f7ti#3+i&b!rss$h8qo9259t3zd
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+import os
+CODESPACE_NAME = os.environ.get('CODESPACE_NAME')
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+if CODESPACE_NAME:
+    ALLOWED_HOSTS.append(f'{CODESPACE_NAME}-8000.app.github.dev')
 
 
 # Application definition
